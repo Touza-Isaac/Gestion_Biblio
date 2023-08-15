16a3b8637e8598f2647b5e715791c41fd0afc4f4
Touza Isaac <62522441+Touza-Isaac@users.noreply.github.com>
2023-08-15 12:32:14 +0100

    add screenshot

--- src/gestion_biblio/Main/Gestion_Adherents.py
+++ src/gestion_biblio/Main/Gestion_Adherents.py
@@ -7,11 +7,15 @@ def GestionAdherent():
   # Layout gauche avec les boutons
   right_layout = [
       [sg.Button('Actualiser',font=('Arial', 12, 'bold'), size=(12, 2))],
-      [sg.Button('Supprimer',font=('Arial', 12, 'bold'),size=(12, 2))]
+      [sg.Button('Modifier',font=('Arial', 12, 'bold'),size=(12, 2))],
+      [sg.Button('Supprimer',font=('Arial', 12, 'bold'),size=(12, 2),button_color='red')]
   ]
 
   layout_add=[
-      [sg.Button('Ajouter',font=('Arial', 12, 'bold'),size=(12, 2))]
+      [sg.Text("")],
+      [sg.Button('Enregistrer',font=('Arial', 12, 'bold'),size=(12, 2),button_color='green')],
+      [sg.Button('Vider',font=('Arial', 12, 'bold'),size=(12, 2),button_color='gray')]
+  
   ]
   
   def getAdherents():
@@ -31,7 +35,7 @@ def GestionAdherent():
   left_layout= [
       [sg.Text('Liste des adhérents',font=('Arial', 12, 'bold'))],
       [sg.Table(values=adherents_list, headings=['Nom', 'Prénom', 'Téléphone', 'Email'], auto_size_columns=True,
-        display_row_numbers=True, justification='center', key='adherents',size=(80, 10),text_color='black', background_color='white')]
+        display_row_numbers=True, justification='left', key='adherents',size=(0,13),text_color='black', background_color='white')]
   ]
 
   # Layout du bas pour le formulaire
@@ -53,7 +57,8 @@ def GestionAdherent():
           sg.Column([[sg.Text('Telephone',font=('Arial', 12, 'bold'),size=(10,1))], 
                     [sg.Text('Email',font=('Arial', 12, 'bold'))]], justification='right'),
                     
-          sg.Column([[sg.InputText(key='telephone',text_color='black', background_color='white',font=('Arial',11, 'bold'))], 
+          sg.Column(
+            [[sg.InputText(key='telephone',text_color='black', background_color='white',font=('Arial',11, 'bold'))], 
                     [sg.InputText(key='email',text_color='black', background_color='white',font=('Arial', 11, 'bold'))]], justification='left')       
       ]
   ]
@@ -62,9 +67,9 @@ def GestionAdherent():
   # Assemblage des layouts  
   layout = [
       [
-          sg.Column(left_layout ),
+          sg.Column(left_layout),
           sg.VerticalSeparator(), 
-          sg.Column(right_layout,size=(180, 200))
+          sg.Column(right_layout,size=(150, 200))
       ],
       [    
       sg.Column(add_layout, element_justification='c')  ,
@@ -72,18 +77,31 @@ def GestionAdherent():
       ]
   ]
 
-  window = sg.Window('Gestion adhérents', layout,size=(700, 450))
+  window = sg.Window('Gestion adhérents', layout,size=(830, 450))
   
   def saveAdherent(nom, prenom, telephone, email):
     conn = sqlite3.connect('users.db')
     c = conn.cursor() 
-    # Créer la table "adherents" s'elle n'existe pas
+     # Créer la table "adherents" s'elle n'existe pas
     c.execute('''CREATE TABLE IF NOT EXISTS adherents (nom TEXT, prenom TEXT, telephone TEXT, email TEXT)''')
-    # Insérer les informations de l'adhérent dans la table
-    c.execute("INSERT INTO adherents (nom, prenom, telephone, email) VALUES (?, ?, ?, ?)",
+    
+    # Vérifier si les informations existent déjà
+    c.execute("SELECT * FROM adherents WHERE (nom=? AND prenom=?) OR (telephone=? AND email=?)",
               (nom, prenom, telephone, email))
+    existing_adherent = c.fetchone()
+    
+    if existing_adherent:
+        # Mettre à jour les autres informations
+        c.execute("UPDATE adherents SET telephone=?, email=? WHERE nom=? AND prenom=?",
+                  (telephone, email, nom, prenom))
+    else:
+        # Insérer les informations de l'adhérent dans la table
+        c.execute("INSERT INTO adherents (nom, prenom, telephone, email) VALUES (?, ?, ?, ?)",
+                  (nom, prenom, telephone, email))
+    
     conn.commit()
     conn.close()
+
   
   def add_adherents():
     nom = values['nom'] 
@@ -126,7 +144,7 @@ def GestionAdherent():
           break
 
       # Ajouter un adherent    
-      if event=="Ajouter":
+      if event=="Enregistrer":
         add_adherents()
       
       #Actualiser la liste
@@ -142,6 +160,27 @@ def GestionAdherent():
         prenom=ad[1]
         supprimer_adherent(nom,prenom,adherents_list)
         
+      if event=="Vider":
+        #Vider les informations
+        window['nom'].update('')
+        window['prenom'].update('')
+        window['telephone'].update('')
+        window['email'].update('')
+      
+      if event=="Modifier":
+        #on recupere les informations selectionnées
+        index = values["adherents"][0]
+        ad=adherents[index]
+        nom=ad[0]
+        prenom=ad[1]
+        telephone=ad[2]
+        email=ad[3]
+        #on remet les informations dans le formulaire
+        window['nom'].update(nom)
+        window['prenom'].update(prenom)
+        window['telephone'].update(telephone)
+        window['email'].update(email)
+      
 
   window.close()
 
